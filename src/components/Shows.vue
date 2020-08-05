790f6258bb3e1a980a6818c242357ffd3cffc0c2
gabriela <gubriana@gmail.com>
2020-08-05 11:52:04 -0400

    Proyecto conectado con bd de Firebase y datos desplegados

---
+++ src/components/Shows.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card">
+          <div class="card-content  yellow darken-1 white-text center-align">
+            <span class="card-title">TV SHOWS</span>
+          </div>
+          <div class="card-content">   
+            <table class="centered responsive-table highlight">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Network</th>
+                  <th>Seasons</th>
+                  <th>Genres</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="show in shows" :key="show.id">
+                  <td>{{show.title}}</td>
+                  <td>{{show.network}}</td>
+                  <td>{{show.numberOfSeasons}}</td>
+                  <td><span v-for="(genre, i) in show.genres" :key="i"> {{ genre }} </span></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { db } from '@/firebase';  
+
+export default {
+  name: 'Shows',
+  data() {
+        return {
+            shows: []        // to start, the list is empty
+        }
+    },
+    firestore() {           // adding this key/function
+        return {
+            shows: db.collection('shows')
+        }
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueFirestore from 'vue-firestore' 
+
+Vue.use(VueFirestore, {key: 'id'});      // adding this use statement
 
 Vue.config.productionTip = false
 
