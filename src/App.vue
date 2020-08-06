fe1bd3ed5da1befd16336358fcd48e6b9050bb0c
gabriela <gubriana@gmail.com>
2020-08-06 10:32:46 -0400

    Formulario reset luego de enviar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
-  <div id="app">
-    <Shows />
+  <div class="container">
+    <div class="row">
+      <div class="col s12 12">
+        <Shows />
+      </div>
+      <div class="col s12 m12">
+        <NewShows />
+      </div>
+    </div>
   </div>
+        
 </template>
 
 <script>
 import Shows from './components/Shows.vue'
+import NewShows from './components/NewShows.vue'
 
 export default {
   name: 'App',
   components: {
-    Shows
+    Shows,
+    NewShows
   }
 }
 </script>
--- src/components/Shows.vue
+++ src/components/Shows.vue
@@ -1,33 +1,27 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col s12 m12">
-        <div class="card">
-          <div class="card-content  yellow darken-1 white-text center-align">
-            <span class="card-title">TV SHOWS</span>
-          </div>
-          <div class="card-content">   
-            <table class="centered responsive-table highlight">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Network</th>
-                  <th>Seasons</th>
-                  <th>Genres</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="show in shows" :key="show.id">
-                  <td>{{show.title}}</td>
-                  <td>{{show.network}}</td>
-                  <td>{{show.numberOfSeasons}}</td>
-                  <td><span v-for="(genre, i) in show.genres" :key="i"> {{ genre }} </span></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+  <div class="card">
+    <div class="card-content  yellow darken-1 white-text center-align">
+      <span class="card-title">TV SHOWS</span>
+    </div>
+    <div class="card-content">   
+      <table class="centered responsive-table highlight">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Network</th>
+            <th>Seasons</th>
+            <th>Genres</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="show in shows" :key="show.id">
+            <td>{{show.title}}</td>
+            <td>{{show.network}}</td>
+            <td>{{show.numberOfSeasons}}</td>
+            <td><span v-for="(genre, i) in show.genres" :key="i"> {{ genre }} </span></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
